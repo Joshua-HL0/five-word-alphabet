7fd4e8776846a02e4dc3843192bf4bc286a32b53
Joshua <joshua.hl0.git@gmail.com>
2024-07-28 16:45:57 +0100

    made basic header file and source template

---
+++ five-word.c
@@ -0,0 +1,10 @@
+#include "five-word.h"
+
+int main(int argc, char *argv[]){
+
+    if (argc < 3){
+        printf("Usage: %s <thread count> <wordlist>\n", argv[0]);
+        return 1;
+    }
+
+}
---
+++ five-word.h
@@ -0,0 +1,17 @@
+#include <cstdint>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdint.h>
+
+#define MaxIntersects 20000
+
+
+typedef struct WordStruct{
+    char Chars[64];
+    uint32_t LetterMask;
+    uint32_t Intersects[MaxIntersects];
+
+} Word;
