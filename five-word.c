f7c63ac5f33e6f2918e09cb2353c156b737ff071
Joshua <joshua.hl0.git@gmail.com>
2024-07-30 15:37:44 +0100

    added file processing system and misc header vars

--- five-word.c
+++ five-word.c
@@ -1,5 +1,6 @@
 #include "five-word.h"
 
+
 int main(int argc, char *argv[]){
 
     if (argc < 3){
@@ -7,4 +8,76 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    uint32_t ThreadCount = atoi(argv[1]);
+    if (ThreadCount < 1 || ThreadCount > MaxThreads){
+        printf("ERR: Thread count must be between 1 and %d\n", MaxThreads);
+        return 1;
+    }
+
+    if (pthread_mutex_init(&CalcMutex, NULL)){
+        printf("ERR: Could not initialise calculation mutex\n");
+        return 1;
+    }
+    if (pthread_mutex_init(&OutputMutex, NULL)){
+        printf("ERR: Could not initialise output mutex\n");
+        return 1;
+    }
+
+    FILE *WordFile = fopen(argv[2], "r");
+    if (!WordFile){
+        printf("ERR: Could not open word file %s\n", argv[2]);
+    }
+
+    AllWords = malloc(sizeof(Word) * MaxWords);
+    if (!AllWords){
+        printf("ERR: Could not allocate main word array\n");
+        return 1;
+    }
+
+    if (LoadWordFile(WordFile) == 1){
+        return 1;
+    }
+    fclose(WordFile);
+        
 }
+
+
+
+uint8_t LoadWordFile(FILE *WordFile){
+    WordCount = 0;
+    uint32_t wordIndex = 0;
+    uint32_t validWord = 1;
+
+    while (fgets(AllWords[wordIndex].Chars, MaxWordLength, WordFile)){
+        char *currentWord = AllWords[wordIndex].Chars;
+        validWord = 1;
+
+        if (currentWord[5] == '\r'){ //if 5 letter word
+            currentWord[5] = 0;
+            for (int i = 0; i < 5; i++){
+                if (currentWord[i] < 'a' || currentWord[i] > 'z'){
+                    printf("ERR: Word file contains invalid chars (Must only contain lowercase a-z)\n");
+                    return 1;
+                }
+                else{
+                    if (AllWords[wordIndex].LetterMask & (1 << (currentWord[i] - 'a'))){
+                        validWord = 0;
+                        break;
+                    }
+                    else{
+                        AllWords[wordIndex].LetterMask |= (1 << (currentWord[i] - 'a'));
+                        printf("Test: %s, %u\n", currentWord, wordIndex);
+                    }
+                }
+            }
+            
+        }
+        if (validWord){
+            wordIndex++;
+        }
+    }
+    printf("Valid word count: %u\n", wordIndex);
+    return 0;
+}
+
+
--- five-word.h
+++ five-word.h
@@ -5,12 +5,24 @@
 #include <string.h>
 #include <stdint.h>
 
-#define MaxIntersects 20000
-
+#define MaxWords 15000
+#define MaxIntersects 10000
+#define MaxWordLength 64
+#define MaxThreads 64
 
 typedef struct WordStruct{
-    char Chars[64];
+    char Chars[MaxWordLength];
     uint32_t LetterMask;
     uint32_t Intersects[MaxIntersects];
 
 } Word;
+
+Word *AllWords;
+Word *WordList; // All valid words
+uint32_t WordCount;
+uint32_t ComboCount;
+pthread_mutex_t CalcMutex;
+pthread_mutex_t OutputMutex;
+
+
+uint8_t LoadWordFile(FILE *WordFile);
